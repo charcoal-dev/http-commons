ca0ff4b63f4a7f29e6875f273c1a8f4c9cd22b26
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-24 21:13:04 +0400

    key/value pairs

---
+++ src/AbstractDataStore.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/http-commons" package.
+ * https://github.com/charcoal-dev/http-commons
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/http-commons/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\HTTP\Commons;
+
+/**
+ * Class AbstractDataStore
+ * @package Charcoal\HTTP\Commons
+ */
+abstract class AbstractDataStore implements \IteratorAggregate
+{
+    protected array $data = [];
+    protected int $count = 0;
+
+    /**
+     * @param string $validateKeyRegExp
+     * @param bool $validateKeys
+     */
+    protected function __construct(
+        protected readonly string $validateKeyRegExp,
+        public bool               $validateKeys = true,
+    )
+    {
+    }
+
+    /**
+     * @return int
+     */
+    public function count(): int
+    {
+        return $this->count;
+    }
+
+    /**
+     * @param \Charcoal\HTTP\Commons\KeyValuePair $pair
+     * @return bool
+     */
+    protected function storeKeyValue(KeyValuePair $pair): bool
+    {
+        if ($this->validateKeys && !preg_match($this->validateKeyRegExp, $pair->key)) {
+            return false;
+        }
+
+        $this->data[strtolower($pair->key)] = $pair;
+        $this->count++;
+        return true;
+    }
+
+    /**
+     * @param string $key
+     * @return \Charcoal\HTTP\Commons\KeyValuePair|null
+     */
+    protected function getValue(string $key): ?KeyValuePair
+    {
+        return $this->data[strtolower($key)] ?? null;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    protected function hasValue(string $key): bool
+    {
+        return isset($this->data[strtolower($key)]);
+    }
+
+    /**
+     * @param string $key
+     * @return void
+     */
+    protected function deleteValue(string $key): void
+    {
+        $key = strtolower($key);
+        if (isset($this->data[$key])) {
+            unset($this->data[$key]);
+            $this->count--;
+        }
+    }
+
+    /**
+     * @return \Traversable
+     */
+    public function getIterator(): \Traversable
+    {
+        return new \ArrayIterator($this->data);
+    }
+}
---
+++ src/KeyValuePair.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/http-commons" package.
+ * https://github.com/charcoal-dev/http-commons
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/http-commons/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\HTTP\Commons;
+
+/**
+ * Class KeyValuePair
+ * @package Charcoal\HTTP\Commons
+ */
+class KeyValuePair
+{
+    /**
+     * @param string $key
+     * @param string|int|float|bool|array|null $value
+     */
+    public function __construct(
+        public readonly string                           $key,
+        public readonly string|int|float|bool|null|array $value
+    )
+    {
+    }
+}
