e51e9e0671346b15489a283a3d31d1011f368bd7
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-24 21:13:11 +0400

    base exception

---
+++ src/Exception/HttpException.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/http-commons" package.
+ * https://github.com/charcoal-dev/http-commons
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/http-commons/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\HTTP\Commons\Exception;
+
+/**
+ * Class HttpException
+ * @package Charcoal\HTTP\Commons\Exception
+ */
+class HttpException extends \Exception
+{
+}
+
