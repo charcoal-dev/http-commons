2fce0ac82bbc9f54cf276f752b34b46bbd1ec679
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 10:30:16 +0500

    Refactor `AbstractDataStore` for key normalization
    Introduce `normalizeSanitizeStoreKey` for consistent key handling across storage methods. Simplify and streamline method implementations, reduce redundancy, and update docblocks for clarity.

--- src/AbstractDataStore.php
+++ src/AbstractDataStore.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/http-commons" package.
- * https://github.com/charcoal-dev/http-commons
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/http-commons/blob/master/LICENSE
+/**
+ * Part of the "charcoal-dev/http-commons" package.
+ * @link https://github.com/charcoal-dev/http-commons
  */
 
 declare(strict_types=1);
@@ -17,47 +11,60 @@ namespace Charcoal\Http\Commons;
 /**
  * Class AbstractDataStore
  * @package Charcoal\Http\Commons
+ * @property array<string,KeyValuePair> $data
  */
 abstract class AbstractDataStore implements \IteratorAggregate
 {
+    /** @var array<string,KeyValuePair> $data */
     protected array $data = [];
     protected int $count = 0;
 
-    /**
-     * @param string $validateKeyRegExp
-     * @param bool $validateKeys
-     */
-    protected function __construct(
-        protected readonly string $validateKeyRegExp,
-        public bool               $validateKeys = true,
-    )
+    abstract protected function sanitizeStoreKey(string $key): string;
+
+    protected function normalizeSanitizeStoreKey(string $key): string
     {
+        return strtolower(trim($this->sanitizeStoreKey($key)));
     }
 
-    /**
-     * @return int
-     */
     public function count(): int
     {
         return $this->count;
     }
 
-    /**
-     * @param string $key
-     * @return bool
-     */
     public function has(string $key): bool
     {
-        return array_key_exists(strtolower($key), $this->data);
+        return array_key_exists($this->normalizeSanitizeStoreKey($key), $this->data);
     }
 
-    /**
-     * @return array
-     */
+    protected function storeKeyValue(KeyValuePair $pair): bool
+    {
+        $this->data[$this->normalizeSanitizeStoreKey($pair->key)] = $pair;
+        $this->count++;
+        return true;
+    }
+
+    protected function getKeyValue(string $key): ?KeyValuePair
+    {
+        return $this->data[$this->normalizeSanitizeStoreKey($key)] ?? null;
+    }
+
+    protected function hasValue(string $key): bool
+    {
+        return isset($this->data[$this->normalizeSanitizeStoreKey($key)]);
+    }
+
+    protected function deleteValue(string $key): void
+    {
+        $key = $this->normalizeSanitizeStoreKey($key);
+        if (isset($this->data[$key])) {
+            unset($this->data[$key]);
+            $this->count--;
+        }
+    }
+
     final public function toArray(): array
     {
         $data = [];
-        /** @var \Charcoal\Http\Commons\KeyValuePair $prop */
         foreach ($this->data as $prop) {
             $data[$prop->key] = $prop->value;
         }
@@ -65,55 +72,6 @@ abstract class AbstractDataStore implements \IteratorAggregate
         return $data;
     }
 
-    /**
-     * @param \Charcoal\Http\Commons\KeyValuePair $pair
-     * @return bool
-     */
-    protected function storeKeyValue(KeyValuePair $pair): bool
-    {
-        if ($this->validateKeys && !preg_match($this->validateKeyRegExp, $pair->key)) {
-            return false;
-        }
-
-        $this->data[strtolower($pair->key)] = $pair;
-        $this->count++;
-        return true;
-    }
-
-    /**
-     * @param string $key
-     * @return \Charcoal\Http\Commons\KeyValuePair|null
-     */
-    protected function getValue(string $key): ?KeyValuePair
-    {
-        return $this->data[strtolower($key)] ?? null;
-    }
-
-    /**
-     * @param string $key
-     * @return bool
-     */
-    protected function hasValue(string $key): bool
-    {
-        return isset($this->data[strtolower($key)]);
-    }
-
-    /**
-     * @param string $key
-     * @return void
-     */
-    protected function deleteValue(string $key): void
-    {
-        $key = strtolower($key);
-        if (isset($this->data[$key])) {
-            unset($this->data[$key]);
-            $this->count--;
-        }
-    }
-
-    /**
-     * @return \Traversable
-     */
     public function getIterator(): \Traversable
     {
         return new \ArrayIterator($this->data);
