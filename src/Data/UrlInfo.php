122a0207ee5265bbbe83bc0f33e9cde355c29d14
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 13:38:39 +0500

    Add `UrlInfo` class and `InvalidUrlException` for URL parsing and validation
    Introduce `UrlInfo` readonly class to encapsulate URL components. Add `InvalidUrlException` to handle invalid URL scenarios during parsing.

---
+++ src/Data/UrlInfo.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Part of the "charcoal-dev/http-commons" package.
+ * @link https://github.com/charcoal-dev/http-commons
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Http\Commons\Data;
+
+use Charcoal\Http\Commons\Exception\InvalidUrlException;
+
+/**
+ * Class UrlInfo
+ * @package Charcoal\Http\Commons
+ */
+readonly class UrlInfo
+{
+    public string $complete;
+    public ?string $scheme;
+    public ?string $host;
+    public ?int $port;
+    public ?string $username;
+    public ?string $password;
+    public ?string $path;
+    public ?string $query;
+    public ?string $fragment;
+
+    public function __construct(string $url)
+    {
+        $parsed = parse_url($url);
+        if (!is_array($parsed) || !$parsed) {
+            throw new InvalidUrlException("Invalid URL");
+        }
+
+        $this->complete = $url;
+        $this->scheme = $parsed["scheme"] ?? null;
+        $this->host = $parsed["host"] ?? null;
+        $this->port = $parsed["port"] ?? null;
+        $this->username = $parsed["user"] ?? null;
+        $this->password = $parsed["pass"] ?? null;
+        $this->path = $parsed["path"] ?? null;
+        $this->query = $parsed["query"] ?? null;
+        $this->fragment = $parsed["fragment"] ?? null;
+    }
+}
---
+++ src/Exception/InvalidUrlException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of the "charcoal-dev/http-commons" package.
+ * @link https://github.com/charcoal-dev/http-commons
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Http\Commons\Exception;
+
+/**
+ * Class InvalidUrlException
+ * @package Charcoal\Http\Commons\Exception
+ */
+class InvalidUrlException extends HttpException
+{
+}
